269e0514be95fc52b1561d3ce3a0202b9dd0aee6
luan-peixoto <66537303+luan-peixoto@users.noreply.github.com>
2021-12-29 13:27:28 -0300

    sadko

--- js/script.js
+++ js/script.js
@@ -1,147 +1,167 @@
 // FUNÇÃO PARA FECHAR AVISO
 
-function fecharAviso() {
-    document.getElementsByClassName("aviso")[0].style.display = "none";
-}
+    function fecharAviso() {
+        document.getElementsByClassName("aviso")[0].style.display = "none";
+    }
 
+// ---------------------
 
 // FUNÇÃO PARA REMOVER NAVBAR MOBILE SE CLICAR FORA DELA:
 
-$(document).click(function (event) {
-    if (!$(event.target).is('.navbar-collapse *')) {
-        $('.navbar-collapse').collapse('hide');
-    }
-});
+    $(document).click(function (event) {
+        if (!$(event.target).is('.navbar-collapse *')) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
 
-function mostrarImagem(id) {
-    var i;
-    var img_principal = document.getElementsByClassName("img-principal")
-    // pega todas as imagens grandes fora do modal
-    for (i = 0; i < img_principal.length ; i++) {
-        img_principal[i].className = img_principal[i].className.replace("d-block", "d-none");
-        // muda o display de todas elas pra none
-    }
-    
-    img_principal[id-1].className = img_principal[id-1].className.replace("d-none", "d-block");
-    // muda o display da imagem atual pra block
+// ----------------------------
 
-    var thumbnails = document.getElementsByClassName("thumb-principal")
-    // pega todas as imagens thumbnail da tela principal
+// FUNÇÃO PARA MOSTRAR O DISPLAY DE IMAGENS FORA DO MOBILE FORA DO LIGHT-BOX
 
-    for (i = 0; i < thumbnails.length ; i++) {
-        thumbnails[i].className = thumbnails[i].className.replace("thumb-active-border", "thumb-demo-border");
-        // muda a borda de todas as thumbnails principais pra borda de não selecionado
-    }
+    function mostrarImagem(id) {
+        var i;
+        var img_principal = document.getElementsByClassName("img-principal")
+        // pega todas as imagens grandes fora do modal
+        for (i = 0; i < img_principal.length ; i++) {
+            img_principal[i].className = img_principal[i].className.replace("d-block", "d-none");
+            // muda o display de todas elas pra none
+        }
+        
+        img_principal[id-1].className = img_principal[id-1].className.replace("d-none", "d-block");
+        // muda o display da imagem atual pra block
 
-    thumbnails[id-1].className = thumbnails[id-1].className.replace("thumb-demo-border", "thumb-active-border");
-    // muda a borda da thumbnail atual para borda de selecionado
-}
+        var thumbnails = document.getElementsByClassName("thumb-principal")
+        // pega todas as imagens thumbnail da tela principal
 
-// getElementByClassName() não existe, só existe getElementsByClassName(), mesmo que seja 1
-// só elemento, é necessário colocar on índice dele [0], pois este metodo retorna um array.
+        for (i = 0; i < thumbnails.length ; i++) {
+            thumbnails[i].className = thumbnails[i].className.replace("thumb-active-border", "thumb-demo-border");
+            // muda a borda de todas as thumbnails principais pra borda de não selecionado
+        }
 
-
-// abrir o modal
-function abrirModal() {
-    document.getElementById("modal-lightbox").style.display = "block";
-    document.body.style.overflow = "hidden";
-    // overflow hidden impede que o documento seja rolado quando o modal do lightbox tá aberto
-}
-
-// fechar o modal
-function fecharModal() {
-    document.getElementById("modal-lightbox").style.display = "none";
-    document.body.style.overflow = "visible";
-    // overflow visible torna o documento rolavel quando o modal do lightbox for fechado
-}
-
-// Next/previous controls
-function proximoSlide(n) {
-    mostrarSlide(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    mostrarSlide(slideIndex = n);
-}
-
-
-// a função show slides coloca 'active' na imagem do slide clicada, usando o parametro 'n'
-function mostrarSlide(n) {
-    var i; // define i
-    var slides = document.getElementsByClassName("modal-image"); 
-    // recupera os slides (imagens grandes)
-    var dots = document.getElementsByClassName("dot"); 
-    // recupera os botões (mini-imagens) do modal
-    var slideNumeroTexto = document.getElementById("slide-number-text"); 
-    // recupera a div que contém o número atual do slide
-    if (n > slides.length) { slideIndex = 1 }
-    // se o botão > foi clicado no último slide, o índice passa a ser slides.length +1, esse if 
-    // faz com que ele volte a 1.
-    if (n < 1) { slideIndex = slides.length }
-    // se o botão < foi clicado no primeiro slide, o índice passa a ser 0, esse if faz com que
-    // ele passe a ser slides.length.
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        // pega todas as imagens do slide e faz com que o display delas seja none
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("thumb-active-border", "thumb-demo-border");
-        // pega todas os botões (mini-imagem) do slide e muda a classe active delas para demo,
-        // ou seja, escurece todas as thumbs
+        thumbnails[id-1].className = thumbnails[id-1].className.replace("thumb-demo-border", "thumb-active-border");
+        // muda a borda da thumbnail atual para borda de selecionado
     }
 
-    slides[slideIndex - 1].style.display = "block";
-    // pega a imagem do slide cujo index seja seja o atual e exibe ela
-    dots[slideIndex - 1].className = dots[slideIndex - 1].className.replace("thumb-demo-border", "thumb-active-border");
-    // pega a thumb (mini-imagem) do slide cujo index seja o atual e troca a classe 'demo' por
-    // 'active', ou seja, ilumina a thumb
-    
-    slideNumeroTexto.innerHTML = slideIndex + "/" + slides.length + " - " + dots[slideIndex - 1].alt;
-    // muda o texto da div pelo número atual do slide e coloca dentro dele a string que 
-    // fica dentro da propriedade 'alt' da thumb atual.
-}
+    // getElementByClassName() não existe, só existe getElementsByClassName(), mesmo que seja 1
+    // só elemento, é necessário colocar on índice dele [0], pois este metodo retorna um array.
 
-var slideIndex = 1;
-// define um valor aleatório pro índice do slide, ao clicar em qualquer uma imagem do lightbox,
-// esse valor vai ser mudado pelo índice da imagem.
+
+// FUNÇÃO PARA ABRIR MODAL DO LIGHTBOX
+
+    function abrirModal() {
+        var lightbox = document.getElementById("modal-lightbox");
+        lightbox.style.display = "block";
+        lightbox.classList.add("fade-in");
+
+        document.body.style.overflow = "hidden";
+        // overflow hidden impede que o documento seja rolado quando o modal do lightbox tá aberto
+    }
+
+// fUNÇÃO PARA FECHAR MODAL DO LIGHTBOX
+    function fecharModal() {
+        var lightbox = document.getElementById("modal-lightbox");
+        lightbox.classList.remove("fade-in");
+        lightbox.classList.add("fade-out");
+        // timeout espera a animação de fade-out terminar para fechar o modal
+        setTimeout(function() {
+            lightbox.style.display = "none";
+            lightbox.classList.remove("fade-out");
+            document.body.style.overflow = "visible";
+            // overflow visible torna o documento rolavel quando o modal do lightbox for fechado
+        }, 300)
+        
+    }
+
+// FUNÇÕES PARA CONTROLAR AS IMAGENS DO MODAL DO LIGHTBOX
+
+    // Next/previous controls
+    function proximoSlide(n) {
+        mostrarSlide(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        mostrarSlide(slideIndex = n);
+    }
+
+
+    // a função show slides coloca 'active' na imagem do slide clicada, usando o parametro 'n'
+    function mostrarSlide(n) {
+        var i; // define i
+        var slides = document.getElementsByClassName("modal-image"); 
+        // recupera os slides (imagens grandes)
+        var dots = document.getElementsByClassName("dot"); 
+        // recupera os botões (mini-imagens) do modal
+        var slideNumeroTexto = document.getElementById("slide-number-text"); 
+        // recupera a div que contém o número atual do slide
+        if (n > slides.length) { slideIndex = 1 }
+        // se o botão > foi clicado no último slide, o índice passa a ser slides.length +1, esse if 
+        // faz com que ele volte a 1.
+        if (n < 1) { slideIndex = slides.length }
+        // se o botão < foi clicado no primeiro slide, o índice passa a ser 0, esse if faz com que
+        // ele passe a ser slides.length.
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            // pega todas as imagens do slide e faz com que o display delas seja none
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace("thumb-active-border", "thumb-demo-border");
+            // pega todas os botões (mini-imagem) do slide e muda a classe active delas para demo,
+            // ou seja, escurece todas as thumbs
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        // pega a imagem do slide cujo index seja seja o atual e exibe ela
+        dots[slideIndex - 1].className = dots[slideIndex - 1].className.replace("thumb-demo-border", "thumb-active-border");
+        // pega a thumb (mini-imagem) do slide cujo index seja o atual e troca a classe 'demo' por
+        // 'active', ou seja, ilumina a thumb
+        
+        slideNumeroTexto.innerHTML = slideIndex + "/" + slides.length + " - " + dots[slideIndex - 1].alt;
+        // muda o texto da div pelo número atual do slide e coloca dentro dele a string que 
+        // fica dentro da propriedade 'alt' da thumb atual.
+    }
+
+    var slideIndex = 1;
+    // define um valor aleatório pro índice do slide, ao clicar em qualquer uma imagem do lightbox,
+    // esse valor vai ser mudado pelo índice da imagem.
 
 
 // FUNÇÃO PARA FECHAR O LIGHT-BOX CLICANDO APENAS NA PARTE PRETA (LUGARES QUE N A CLASSE fechar-modal)
 
-$('.modal-lightbox').on('click', function(e) {
-    if(!$(e.target).hasClass('fechar-modal')){ // check if target is not the image displayed
-      fecharModal();
-      console.log("pies")
-    } 
- });
+    $('.modal-lightbox').on('click', function(e) {
+        if(!$(e.target).hasClass('fechar-modal')){ // check if target is not the image displayed
+        fecharModal();
+        console.log("pies")
+        } 
+    });
+
+// ----------------------
 
 
 // FUNÇÕES DO DISPLAY DE IMAGENS MOBILE
 
-var slideIndexMobile = 1;
+    var slideIndexMobile = 1;
 
-function proximoSlideMobile(n) {
-    console.log("te")
-    mostrarSlideMobile(slideIndexMobile += n);
-}
-
-function mostrarSlideMobile(id) {
-    var i; // define i
-    var slides_mobile = document.getElementsByClassName("img-principal-mobile"); 
-    // recupera as imagens do display mobile
-    if (id > slides_mobile.length) { slideIndexMobile = 1 }
-    // se o botão > foi clicado no último slide, o índice passa a ser slides.length +1, esse if 
-    // faz com que ele volte a 1.
-    if (id < 1) { slideIndexMobile = slides_mobile.length }
-    // se o botão < foi clicado no primeiro slide, o índice passa a ser 0, esse if faz com que
-    // ele passe a ser slides.length.
-    for (i = 0; i < slides_mobile.length; i++) {
-        slides_mobile[i].className = slides_mobile[i].className.replace("d-block", "d-none");
-        // pega todas as imagens do display e muda de d-block para d-none
+    function proximoSlideMobile(n) {
+        mostrarSlideMobile(slideIndexMobile += n);
     }
 
-    slides_mobile[slideIndexMobile - 1].className = slides_mobile[slideIndexMobile - 1].className.replace("d-none", "d-block");
-    // pega a imagem cujo index seja seja o atual e exibe ela
-}
+    function mostrarSlideMobile(id) {
+        var i; // define i
+        var slides_mobile = document.getElementsByClassName("img-principal-mobile"); 
+        // recupera as imagens do display mobile
+        if (id > slides_mobile.length) { slideIndexMobile = 1 }
+        // se o botão > foi clicado no último slide, o índice passa a ser slides.length +1, esse if 
+        // faz com que ele volte a 1.
+        if (id < 1) { slideIndexMobile = slides_mobile.length }
+        // se o botão < foi clicado no primeiro slide, o índice passa a ser 0, esse if faz com que
+        // ele passe a ser slides.length.
+        for (i = 0; i < slides_mobile.length; i++) {
+            slides_mobile[i].className = slides_mobile[i].className.replace("d-block", "d-none");
+            // pega todas as imagens do display e muda de d-block para d-none
+        }
+
+        slides_mobile[slideIndexMobile - 1].className = slides_mobile[slideIndexMobile - 1].className.replace("d-none", "d-block");
+        // pega a imagem cujo index seja seja o atual e exibe ela
+    }
 
